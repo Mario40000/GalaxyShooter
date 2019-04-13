9d799c4e0b5262f5f4e5f0140d18018bf1131308
Mario40000 <mario40000@hotmail.com>
2019-04-13 07:08:31 +0200

    Pantalla de titulo y empezamos a implementar el coop mode

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    //Metodos para cargar las escenas
+
+    public void LoadSingle ()
+    {
+        SceneManager.LoadScene("SinglePlayer");
+    }
+
+    public void LoadCoop ()
+    {
+        SceneManager.LoadScene("CoopMode");
+    }
+	
+}
